d2350c29343bc966aee3fb918711dc4397b56cfe
RobertRathbone <BobRathbone@gmail.com>
2021-11-24 13:32:03 -0800

    Validations

--- server/models/blog.model.js
+++ server/models/blog.model.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 
 const BlogSchema = new mongoose.Schema({
-    title: String,
-    body: String,
-    description: String
+    title: {
+        type: String,
+        required: [true, "A title is required"],
+    },
+    body: {
+        type: String,
+        required: [true, "A blog is not a blog without some words here..."],
+    },
+    description: {
+        type: String,
+        required: [true, "A short synopsis is not just appreciated, but required here."],
+    },
 })
 
 module.exports.Blog = mongoose.model("Blog", BlogSchema);
